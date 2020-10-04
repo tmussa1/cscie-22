2799a0ec97097f69a8b79f915049cdeac492b9a6
Tofik Mussa <tofahub@gmail.com>
2020-10-03 20:23:28 -0400

    setup pset2

---
+++ pset-1/ArrayBag.java
@@ -0,0 +1,327 @@
+/* 
+ * ArrayBag.java
+ * 
+ * Computer Science E-22
+ *
+ * Modified by: Tofik Mussa, tmussa@g.harvard.edu
+ */
+
+import java.util.*;
+
+/**
+ * An implementation of the Bag ADT using an array.
+ */
+public class ArrayBag implements Bag {
+    /** 
+     * The array used to store the items in the bag.
+     */
+    private Object[] items;
+    
+    /** 
+     * The number of items in the bag.
+     */
+    private int numItems;
+    
+    public static final int DEFAULT_MAX_SIZE = 50;
+    
+    /**
+     * Constructor with no parameters - creates a new, empty ArrayBag with 
+     * the default maximum size.
+     */
+    public ArrayBag() {
+        this.items = new Object[DEFAULT_MAX_SIZE];
+        this.numItems = 0;
+    }
+    
+    /** 
+     * A constructor that creates a new, empty ArrayBag with the specified
+     * maximum size.
+     */
+    public ArrayBag(int maxSize) {
+        if (maxSize <= 0) {
+            throw new IllegalArgumentException("maxSize must be > 0");
+        }
+        this.items = new Object[maxSize];
+        this.numItems = 0;
+    }
+    
+    /**
+     * numItems - accessor method that returns the number of items 
+     * in this ArrayBag.
+     */
+    public int numItems() {
+        return this.numItems;
+    }
+    
+    /** 
+     * add - adds the specified item to this ArrayBag. Returns true if there 
+     * is room to add it, and false otherwise.
+     * Throws an IllegalArgumentException if the item is null.
+     */
+    public boolean add(Object item) {
+        if (item == null) {
+            throw new IllegalArgumentException("item must be non-null");
+        } else if (this.numItems == this.items.length) {
+            return false;    // no more room!
+        } else {
+            this.items[this.numItems] = item;
+            this.numItems++;
+            return true;
+        }
+    }
+    
+    /** 
+     * remove - removes one occurrence of the specified item (if any)
+     * from this ArrayBag.  Returns true on success and false if the
+     * specified item (i.e., an object equal to item) is not in this ArrayBag.
+     */
+    public boolean remove(Object item) {
+        for (int i = 0; i < this.numItems; i++) {
+            if (this.items[i].equals(item)) {
+                // Shift the remaining items left by one.
+                for (int j = i; j < this.numItems - 1; j++) {
+                    this.items[j] = this.items[j + 1];
+                }
+                this.items[this.numItems - 1] = null;
+                
+                this.numItems--;
+                return true;
+            }
+        }
+        
+        return false;  // item not found
+    }
+    
+    /**
+     * contains - returns true if the specified item is in the Bag, and
+     * false otherwise.
+     */
+    public boolean contains(Object item) {
+        for (int i = 0; i < this.numItems; i++) {
+            if (this.items[i].equals(item)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * containsAll - does this ArrayBag contain all of the items in
+     * otherBag?  Returns false if otherBag is null or empty. 
+     */
+    public boolean containsAll(Bag otherBag) {
+        if (otherBag == null || otherBag.numItems() == 0) {
+            return false;
+        }
+        
+        Object[] otherItems = otherBag.toArray();
+        for (int i = 0; i < otherItems.length; i++) {
+            if (! this.contains(otherItems[i])) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    /**
+     * grab - returns a reference to a randomly chosen item in this ArrayBag.
+     */
+    public Object grab() {
+        if (this.numItems == 0) {
+            throw new IllegalStateException("the bag is empty");
+        }
+        
+        int whichOne = (int)(Math.random() * this.numItems);
+        return this.items[whichOne];
+    }
+    
+    /**
+     * toArray - return an array containing the current contents of the bag
+     */
+    public Object[] toArray() {
+        Object[] copy = new Object[this.numItems];
+        
+        for (int i = 0; i < this.numItems; i++) {
+            copy[i] = this.items[i];
+        }
+        
+        return copy;
+    }
+
+    @Override
+    public int capacity() {
+        return this.items.length;
+    }
+
+    @Override
+    public boolean isFull() {
+        return numItems() == capacity();
+    }
+
+    @Override
+    public void increaseCapacity(int amount) {
+
+        if(amount < 0){
+            throw new IllegalArgumentException("Amount can't be negative");
+        } else if(amount == 0){
+            return;
+        }
+
+        Object [] copiedArray = new Object[capacity() + amount];
+
+        for(int i = 0; i < numItems(); i++){
+            copiedArray[i] = this.items[i];
+        }
+
+        this.items = copiedArray;
+    }
+
+    @Override
+    public boolean removeItems(Bag other) {
+
+        if(other == null){
+            throw new IllegalArgumentException("Item to remove is empty");
+        } else if(other.numItems() == 0){
+            return false;
+        }
+
+        Object [] otherItems = other.toArray();
+        boolean isRemoved = false;
+
+        for(int i  = 0; i < other.numItems(); i++){
+
+            while(this.contains(otherItems[i])){
+
+                this.remove(otherItems[i]);
+                isRemoved = true;
+            }
+        }
+
+        return isRemoved;
+    }
+
+    @Override
+    public Bag unionWith(Bag other) {
+
+        if(other == null){
+            throw new IllegalArgumentException("The parameter can not be empty");
+        } else if(other.numItems() == 0 && this.numItems() == 0){
+            return new ArrayBag();
+        }
+
+        ArrayBag result = new ArrayBag(other.capacity() + this.capacity());
+        Object [] otherArray = other.toArray();
+        Object [] thisArray = this.toArray();
+
+        for(int i = 0, k = 0; i < other.numItems() || k < this.numItems; i++, k++){
+
+            if(i < other.numItems() && !result.contains(otherArray[i])){
+                result.add(otherArray[i]);
+            }
+
+            if(k < this.numItems && !result.contains(thisArray[k])){
+                result.add(thisArray[k]);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * toString - converts this ArrayBag into a string that can be printed.
+     * Overrides the version of this method inherited from the Object class.
+     */
+    public String toString() {
+        String str = "{";
+        
+        for (int i = 0; i < this.numItems; i++) {
+            str = str + this.items[i];
+            if (i != this.numItems - 1) {
+                str += ", ";
+            }
+        }
+        
+        str = str + "}";
+        return str;
+    }
+    
+    /* Test the ArrayBag implementation. */
+    public static void main(String[] args) {
+        // Create a Scanner object for user input.
+        Scanner scan = new Scanner(System.in);
+        
+        // Create an ArrayBag named bag1.
+        System.out.print("size of bag 1: ");
+        int size = scan.nextInt();
+        ArrayBag bag1 = new ArrayBag(size);
+        scan.nextLine();    // consume the rest of the line
+        
+        // Read in strings, add them to bag1, and print out bag1.
+        String itemStr;        
+        for (int i = 0; i < size; i++) {
+            System.out.print("item " + i + ": ");
+            itemStr = scan.nextLine();
+            bag1.add(itemStr);
+        }
+//        System.out.println("bag 1 = " + bag1);
+//        System.out.println();
+//
+//        System.out.println("Increase Capacity By: ");
+//        int amount = scan.nextInt();
+//
+//        bag1.increaseCapacity(amount);
+//
+//        System.out.println("New size " + bag1.capacity());
+//
+//        Bag other = new ArrayBag();
+//
+//        System.out.println("Other : ");
+//
+//        for (int i = 1; i < 6; i += 2) {
+//            System.out.println(i);
+//            other.add(i + "");
+//        }
+//        bag1.removeItems(other);
+//
+//        System.out.println("Removing other :");
+//
+//        Object[] items = bag1.toArray();
+//        for (int i = 0; i < items.length; i++) {
+//            System.out.println(items[i]);
+//        }
+//
+//        System.out.println("Union with : ");
+//
+//        bag1 = (ArrayBag) bag1.unionWith(other);
+//
+//        Object [] result = bag1.toArray();
+//
+//        for (int i = 0; i < result.length; i++) {
+//            System.out.println(result[i]);
+//        }
+        
+        // Select a random item and print it.
+        Object item = bag1.grab();
+        System.out.println("grabbed " + item);
+        System.out.println();
+        
+        // Iterate over the objects in bag1, printing them one per
+        // line.
+        Object[] res = bag1.toArray();
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
+        System.out.println();
+        
+        // Get an item to remove from bag1, remove it, and reprint the bag.
+        System.out.print("item to remove: ");
+        itemStr = scan.nextLine();
+        if (bag1.contains(itemStr)) {
+            bag1.remove(itemStr);
+        }
+        System.out.println("bag 1 = " + bag1);
+        System.out.println();
+    }
+}
---
+++ pset-1/MagicSquare.java
@@ -0,0 +1,175 @@
+/* 
+ * MagicSquare.java
+ * 
+ * Computer Science E-22
+ * 
+ * Modified by: Tofik Mussa, tmussa@g.harvard.edu
+ */
+
+import java.util.*;
+
+public class MagicSquare {
+
+    private int[][] values;
+
+    private int [] colSums;
+    private int [] rowSums;
+    private int [] usedValues;
+    private int usedValuesCount;
+    private int order;
+    private int possibleValues;
+
+    public MagicSquare(int order) {
+        this.values = new int[order][order];
+        this.order = order;
+        this.colSums = new int[order];
+        this.rowSums = new int[order];
+        this.usedValues = new int[(this.order * this.order)];
+        this.usedValuesCount = 0;
+        this.possibleValues = (int) Math.pow(this.order, 2);
+
+        for(int i = 0; i < order; i++){
+            this.colSums[i] = (int) (((Math.pow(order, 3)) + order) /2);
+            this.rowSums[i] = (int) (((Math.pow(order, 3)) + order) /2);
+        }
+    }
+
+    private boolean isPossible(int row, int col, int val){
+
+        if(isContained(val) || this.values[row][col] != 0){
+            return false;
+        }
+
+
+        if(col == order - 1 && this.rowSums[row] - val != 0) {
+                return false;
+        }
+
+        if(row == order - 1 && this.colSums[col] - val != 0){
+            return false;
+        }
+
+        if(this.colSums[col] - val > 0 && this.rowSums[row] - val > 0){
+            return true;
+        }
+
+        return true;
+    }
+
+    private boolean isContained(int val){
+
+        for(int i = 0; i < this.usedValuesCount; i++){
+            if(this.usedValues[i] == val){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void placeValue(int row, int col, int val){
+        this.values[row][col] = val;
+        this.colSums[col] -= val;
+        this.rowSums[row] -= val;
+        this.usedValues[this.usedValuesCount] = val;
+        this.usedValuesCount++;
+    }
+
+    private void removeValue(int row, int col, int val){
+        this.values[row][col] = 0;
+        this.colSums[col] += val;
+        this.rowSums[row] += val;
+        this.removeElementAndShiftArray(val);
+    }
+
+    private void removeElementAndShiftArray(int val) {
+
+       int index = 0;
+
+       for(int i = 0; i < this.usedValuesCount; i++) {
+           if (this.usedValues[i] == val) {
+               index = i;
+           }
+       }
+
+       for(int k = index; k < this.usedValuesCount - 1; k++){
+           this.usedValues[k] = this.usedValues[k + 1];
+       }
+
+       this.usedValuesCount--;
+    }
+
+
+    public boolean solveMagicSquare(int row, int colParam){
+
+        if(row == this.order){
+            return true;
+        }
+
+        for (int val = 1; val <= this.possibleValues; val++) {
+
+            if (this.isPossible(row, colParam, val)){
+
+                this.placeValue(row, colParam, val);
+
+                if(colParam == this.order - 1){
+                    if(solveMagicSquare(row + 1, 0)){
+                        return true;
+                    }
+                } else {
+                    if(solveMagicSquare(row,colParam + 1)){
+                        return true;
+                    }
+                }
+
+                this.removeValue(row, colParam, val);
+            }
+        }
+        return false;
+    }
+
+
+    public boolean solve() {
+        return solveMagicSquare(0, 0);
+    }
+
+    public void display() {
+        for (int r = 0; r < order; r++) {
+            printRowSeparator();
+            for (int c = 0; c < order; c++) {
+                System.out.print("|");
+                if (values[r][c] == 0) {
+                    System.out.print("   ");
+                } else {
+                    if (values[r][c] < 10) {
+                        System.out.print(" ");
+                    }
+                    System.out.print(" " + values[r][c] + " ");
+                }
+            }
+            System.out.println("|");
+        }
+        printRowSeparator();
+    }
+
+    private void printRowSeparator() {
+        for (int i = 0; i < order; i++) {
+            System.out.print("-----");
+        }
+        System.out.println("-");
+    }
+    
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+        System.out.print("What order Magic Square? ");
+        int order = console.nextInt();
+
+        MagicSquare puzzle = new MagicSquare(order);
+        if (puzzle.solve()) {
+            System.out.println("Here's the solution:");
+            puzzle.display();
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
